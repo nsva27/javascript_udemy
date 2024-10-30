8c313aeb5fe4c5cdfa4a90b891f19f4de5f0e616
Valery <nesterovsky.valery@gmail.com>
2024-10-30 20:05:26 +0300

    create movieDB

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let a = prompt('Один из последних просмотренных фильмов', ''),
+    b = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+
+console.log(personalMovieDB);
