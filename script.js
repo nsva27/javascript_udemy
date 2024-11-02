2e836dc0cd02fd2bb8837ca1fe6a300827b35864
Valery <nesterovsky.valery@gmail.com>
2024-11-02 09:04:36 +0300

    for-loop task

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 'use strict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
 
-let a = prompt('Один из последних просмотренных фильмов', ''),
-  b = +prompt('На сколько оцените его?', '');
+// let a = prompt('Один из последних просмотренных фильмов', ''),
+//   b = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[a] = b;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+let result = '';
+let strLength = 7;
+
+for (let i = 1; i < strLength; i++) {
+  for (let j = 0; j < i; j++) {
+    result += '*';
+  }
+  result += '\n';
+}
+
+console.log(result);
+// result ->
+// *
+// **
+// ***
+// ****
+// *****
+// ******
