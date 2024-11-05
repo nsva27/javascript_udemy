f77baa87a6f66c06bff60c2063f7d4b0e5aee0f3
Valery <nesterovsky.valery@gmail.com>
2024-11-05 12:36:54 +0300

    solved task with figure

--- script.js
+++ script.js
@@ -49,3 +49,28 @@
 //     }
 //   }
 // }
+
+// task: make figure using code
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//   for (let j = 0; j < lines - i; j++) {
+//     result += ' ';
+//   }
+//   for (let k = 0; k < i * 2 + 1; k++) {
+//     result += "*";
+//   }
+//   result += '\n';
+// }
+
+// console.log(result);
+// result ->
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+
