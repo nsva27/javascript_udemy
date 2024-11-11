baeb8386430be3910f15eb9df1ceb5986551441f
Valery <nesterovsky.valery@gmail.com>
2024-11-11 17:28:44 +0300

    personal movie data base

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 'use strict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
 
-let a = prompt('Один из последних просмотренных фильмов', ''),
-    b = +prompt('На сколько оцените его?', '');
+// let a = prompt('Один из последних просмотренных фильмов', ''),
+//     b = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[a] = b;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+
