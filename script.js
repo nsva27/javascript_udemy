426b4888e4e658276b00687320a76e95c9a269cb
Valery <nesterovsky.valery@gmail.com>
2024-11-11 17:26:43 +0300

    solved third task

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 'use strict';
 
 // Записываем ответ пользователя в переменную
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// Персональная база фильмов
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
+// // Персональная база фильмов
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
 
-// Выводим сообщение в зависимости от количества просмотренных фильмов
-if (personalMovieDB.count <= 10) {
-  console.log('Просмотрено довольно мало фильмов.');
-} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
-  console.log('Вы классический зритель.');
-} else if (personalMovieDB.count > 30) {
-  console.log('Вы киноман!');
-} else {
-  console.log('Произошла ошибка.');
-}
+// // Выводим сообщение в зависимости от количества просмотренных фильмов
+// if (personalMovieDB.count <= 10) {
+//   console.log('Просмотрено довольно мало фильмов.');
+// } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+//   console.log('Вы классический зритель.');
+// } else if (personalMovieDB.count > 30) {
+//   console.log('Вы киноман!');
+// } else {
+//   console.log('Произошла ошибка.');
+// }
 
-// Повторяем вопросы пользователю, если его ответ не подходит по условию
-for (let i = 0; i < 2; i++) {
-  const movieTitle = prompt('Один из последних просмотренных фильмов', '');
-  const movieRate = +prompt('На сколько оцените его?', '');
+// // Повторяем вопросы пользователю, если его ответ не подходит по условию
+// for (let i = 0; i < 2; i++) {
+//   const movieTitle = prompt('Один из последних просмотренных фильмов', '');
+//   const movieRate = +prompt('На сколько оцените его?', '');
 
-  if (movieTitle !== '' && movieRate !== '' && movieTitle !== null && movieRate !== null && movieTitle.length <= 50) {
-    personalMovieDB.movies[movieTitle] = movieRate;
-  } else {
-    i--;
-  }
-}
+//   if (movieTitle !== '' && movieRate !== '' && movieTitle !== null && movieRate !== null && movieTitle.length <= 50) {
+//     personalMovieDB.movies[movieTitle] = movieRate;
+//   } else {
+//     i--;
+//   }
+// }
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
 // Tasks (practice)
 
@@ -95,3 +95,46 @@ console.log(personalMovieDB);
 //  *********
 // ***********
 
+// Functions
+
+function showFirstMessage(message) {
+  console.log(message);
+}
+
+showFirstMessage('Hello world');
+
+
+const usdCurr = 28;
+const eurCurr = 32;
+const discount = 0.9;
+
+function convert(amount, curr) {
+  return amount * curr;
+}
+
+function promotion(result) {
+  console.log(result * discount);
+}
+
+promotion(convert(500, eurCurr));
+
+
+// task 3
+function getMathResult(num, count) {
+  let str = '';
+
+  if (typeof count !== 'number' || count <= 0) {
+    return num;
+  }
+
+  for (let i = 1; i <= count; i++) {
+    if (i !== count) {
+      str += `${num * i}---`;
+    } else {
+      str += `${num * i}`;
+    }
+  }
+  return str;
+}
+
+console.log(getMathResult(5, 50));
