bc14875a2c433486768663fa9606d1eda4b021c3
Valery <nesterovsky.valery@gmail.com>
2024-11-12 13:54:37 +0300

    used functions in my data base

--- script.js
+++ script.js
@@ -1,44 +1,94 @@
 'use strict';
 
-// Записываем ответ пользователя в переменную
-// let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// Personal movie data base
+// Количество просмотренных фильмов
+let numberOfFilms;
 
-// // Персональная база фильмов
-// const personalMovieDB = {
-//   count: numberOfFilms,
-//   movies: {},
-//   actors: {},
-//   genres: [],
-//   privat: false,
-// };
+// Записать ответ пользователя в переменную
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// // Выводим сообщение в зависимости от количества просмотренных фильмов
-// if (personalMovieDB.count <= 10) {
-//   console.log('Просмотрено довольно мало фильмов.');
-// } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
-//   console.log('Вы классический зритель.');
-// } else if (personalMovieDB.count > 30) {
-//   console.log('Вы киноман!');
-// } else {
-//   console.log('Произошла ошибка.');
-// }
+  // Повторить вопрос, если ответ соответствует условию
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
 
-// // Повторяем вопросы пользователю, если его ответ не подходит по условию
-// for (let i = 0; i < 2; i++) {
-//   const movieTitle = prompt('Один из последних просмотренных фильмов', '');
-//   const movieRate = +prompt('На сколько оцените его?', '');
+// start();
 
-//   if (movieTitle !== '' && movieRate !== '' && movieTitle !== null && movieRate !== null && movieTitle.length <= 50) {
-//     personalMovieDB.movies[movieTitle] = movieRate;
-//   } else {
-//     i--;
-//   }
-// }
+// Персональная база фильмов
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
 
-// console.log(personalMovieDB);
+// Записать в массив любимые жанры фильмов
+function writeYourGenres(genres) {
+  for (let i = 0; i < 3; i++) {
+    const genre = prompt(`Ваш любимый жанр под номером ${i + 1}?`, '');
+
+    // if (genre === null || genre === '') {
+    //   i--;
+    // } else {
+    //   genres[i] = genre;
+    // }
+    (genre === null || genre == '') ? i-- : genres[i] = genre;
+  }
+}
+
+// writeYourGenres(personalMovieDB.genres);
+
+// Записать ответы пользователя в объект в формате ключ - значение
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const movieTitle = prompt('Один из последних просмотренных фильмов', '');
+    const movieRate = +prompt('На сколько оцените его?', '');
+
+    // Повторить вопросы, если ответ не подходит по условию
+    if (movieTitle !== '' && movieRate !== '' && movieTitle !== null && movieRate !== null && movieTitle.length <= 50 && typeof movieRate === 'number' && !isNaN(movieRate)) {
+      personalMovieDB.movies[movieTitle] = movieRate;
+    } else {
+      i--;
+    }
+  }
+}
+
+// rememberMyFilms();
+
+// Определить уровень пользователя
+function detectPersonalLevel() {
+  // Вывести сообщение в зависимости от количества просмотренных фильмов
+  if (personalMovieDB.count <= 10) {
+    console.log('Просмотрено довольно мало фильмов.');
+  } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель.');
+  } else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман!');
+  } else {
+    console.log('Произошла ошибка.');
+  }
+}
+
+// detectPersonalLevel();
+
+// Показать объект, если он не приватный
+function showMyDB(hidden) {
+  // if (!hidden) {
+  //   console.log(personalMovieDB);
+  // } else {
+  //   console.log('Sorry, data base is privat.');
+  // }
+  // То же условие, но с тернарным оператором
+  (!hidden) ? console.log(personalMovieDB) : console.log('Sorry, data base is privat.');
+}
+
+showMyDB(personalMovieDB.privat);
+
 
 // Tasks (practice)
-
 // task: make figure using loop
 // let result = '';
 // let strLength = 7;
@@ -71,8 +121,8 @@
 //   }
 // }
 
-// task 2: make figure using loop
 
+// task 2: make figure using loop
 // const lines = 5;
 // let result = '';
 
@@ -95,46 +145,46 @@
 //  *********
 // ***********
 
+
 // Functions
+// function showFirstMessage(message) {
+//   console.log(message);
+// }
 
-function showFirstMessage(message) {
-  console.log(message);
-}
-
-showFirstMessage('Hello world');
-
-
-const usdCurr = 28;
-const eurCurr = 32;
-const discount = 0.9;
-
-function convert(amount, curr) {
-  return amount * curr;
-}
-
-function promotion(result) {
-  console.log(result * discount);
-}
-
-promotion(convert(500, eurCurr));
+// showFirstMessage('Hello world');
 
 
-// task 3
-function getMathResult(num, count) {
-  let str = '';
+// const usdCurr = 28;
+// const eurCurr = 32;
+// const discount = 0.9;
 
-  if (typeof count !== 'number' || count <= 0) {
-    return num;
-  }
+// function convert(amount, curr) {
+//   return amount * curr;
+// }
 
-  for (let i = 1; i <= count; i++) {
-    if (i !== count) {
-      str += `${num * i}---`;
-    } else {
-      str += `${num * i}`;
-    }
-  }
-  return str;
-}
+// function promotion(result) {
+//   console.log(result * discount);
+// }
 
-console.log(getMathResult(5, 50));
+// promotion(convert(500, eurCurr));
+
+
+// // task 3
+// function getMathResult(num, count) {
+//   let str = '';
+
+//   if (typeof count !== 'number' || count <= 0) {
+//     return num;
+//   }
+
+//   for (let i = 1; i <= count; i++) {
+//     if (i !== count) {
+//       str += `${num * i}---`;
+//     } else {
+//       str += `${num * i}`;
+//     }
+//   }
+//   return str;
+// }
+
+// console.log(getMathResult(5, 50));
