4608abcb88646c8f7eaf2fdcce71a6b0b16e3c01
Valery <nesterovsky.valery@gmail.com>
2024-12-24 19:31:50 +0300

    made methods from functions

--- script.js
+++ script.js
@@ -1,91 +1,55 @@
 'use strict';
 
-// Personal movie data base
-// Количество просмотренных фильмов
-let numberOfFilms;
-
-// Записать ответ пользователя в переменную
-function start() {
-  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-  // Повторить вопрос, если ответ соответствует условию
-  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-  }
-}
-
-// start();
-
 // Персональная база фильмов
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
   privat: false,
+  start: function() {
+    this.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (this.count == '' || this.count == null || isNaN(this.count)) {
+      this.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+  },
+  rememberMyFilms: function() {
+    for (let i = 0; i < 2; i++) {
+      const movieTitle = prompt('Один из последних просмотренных фильмов', '');
+      const movieRate = +prompt('На сколько оцените его?', '');
+  
+      if (movieTitle !== '' && movieRate !== '' && movieTitle !== null && movieRate !== null && movieTitle.length <= 50 && typeof movieRate === 'number' && !isNaN(movieRate)) {
+        this.movies[movieTitle] = movieRate;
+      } else {
+        i--;
+      }
+    }
+  },
+  detectPersonalLevel: function() {
+    if (this.count <= 10) {
+      console.log('Просмотрено довольно мало фильмов.');
+    } else if (this.count > 10 && this.count <= 30) {
+      console.log('Вы классический зритель.');
+    } else if (this.count > 30) {
+      console.log('Вы киноман!');
+    } else {
+      console.log('Произошла ошибка.');
+    }
+  },
+  writeYourGenres: function(genres) {
+    for (let i = 0; i < 3; i++) {
+      const genre = prompt(`Ваш любимый жанр под номером ${i + 1}?`, '');
+ 
+      (genre === null || genre == '') ? i-- : genres[i] = genre;
+    }
+  },
+  showMyDB: function(hidden) {
+    (!hidden) ? console.log(this) : console.log('Sorry, data base is privat.');
+  },
 };
 
-// Записать в массив любимые жанры фильмов
-function writeYourGenres(genres) {
-  for (let i = 0; i < 3; i++) {
-    const genre = prompt(`Ваш любимый жанр под номером ${i + 1}?`, '').trim();
-
-    // if (genre === null || genre === '') {
-    //   i--;
-    // } else {
-    //   genres[i] = genre;
-    // }
-    (genre === null || genre == '') ? i-- : genres[i] = genre;
-  }
-}
-
-// writeYourGenres(personalMovieDB.genres);
-
-// Записать ответы пользователя в объект в формате ключ - значение
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    const movieTitle = prompt('Один из последних просмотренных фильмов', '').trim();
-    const movieRate = +prompt('На сколько оцените его?', '').trim();
-
-    // Повторить вопросы, если ответ не подходит по условию
-    if (movieTitle !== '' && movieRate !== '' && movieTitle !== null && movieRate !== null && movieTitle.length <= 50 && typeof movieRate === 'number' && !isNaN(movieRate)) {
-      personalMovieDB.movies[movieTitle] = movieRate;
-    } else {
-      i--;
-    }
-  }
-}
-
-// rememberMyFilms();
-
-// Определить уровень пользователя
-function detectPersonalLevel() {
-  // Вывести сообщение в зависимости от количества просмотренных фильмов
-  if (personalMovieDB.count <= 10) {
-    console.log('Просмотрено довольно мало фильмов.');
-  } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
-    console.log('Вы классический зритель.');
-  } else if (personalMovieDB.count > 30) {
-    console.log('Вы киноман!');
-  } else {
-    console.log('Произошла ошибка.');
-  }
-}
-
-// detectPersonalLevel();
-
-// Показать объект, если он не приватный
-function showMyDB(hidden) {
-  // if (!hidden) {
-  //   console.log(personalMovieDB);
-  // } else {
-  //   console.log('Sorry, data base is privat.');
-  // }
-  // То же условие, но с тернарным оператором
-  (!hidden) ? console.log(personalMovieDB) : console.log('Sorry, data base is privat.');
-}
-
-showMyDB(personalMovieDB.privat);
+personalMovieDB.showMyDB(this.privat);
 
 
 // Задачи, практика.
@@ -121,7 +85,6 @@ showMyDB(personalMovieDB.privat);
 //   }
 // }
 
-
 // Задача. Сделать фигуру 2, используя вложенные циклы.
 // const lines = 5;
 // let result = '';
@@ -145,15 +108,7 @@ showMyDB(personalMovieDB.privat);
 //  *********
 // ***********
 
-
 // Функции.
-// function showFirstMessage(message) {
-//   console.log(message);
-// }
-
-// showFirstMessage('Hello world');
-
-
 // const usdCurr = 28;
 // const eurCurr = 32;
 // const discount = 0.9;
@@ -168,8 +123,7 @@ showMyDB(personalMovieDB.privat);
 
 // promotion(convert(500, eurCurr));
 
-
-// // task 3
+// // Task 3
 // function getMathResult(num, count) {
 //   let str = '';
 
@@ -189,7 +143,6 @@ showMyDB(personalMovieDB.privat);
 
 // console.log(getMathResult(5, 10));
 
-
 // Udemy 27. Методы строк и чисел.
 // const str = 'test';
 // const fruit = 'some fruit';
@@ -201,7 +154,6 @@ showMyDB(personalMovieDB.privat);
 // console.log(logg.slice(0, 5));
 // console.log(parseInt(test));
 // console.log(parseFloat(test));
-
 
 // Задача. Вернуть номер купе, по номеру места.
 // function getCoupeNumber(placeNumber) {
@@ -234,7 +186,6 @@ showMyDB(personalMovieDB.privat);
 
 // getCoupeNumber(12);
 
-
 // Задача. Получить целое число, вернуть результат в часах и минутах.
 // function getTimeFromMinutes(minutesTotal) {
 //   if (typeof minutesTotal !== 'number' || !Number.isInteger(minutesTotal) || minutesTotal < 0) {
@@ -266,7 +217,6 @@ showMyDB(personalMovieDB.privat);
 
 // console.log(getTimeFromMinutes(190));
 
-
 // Задача. Вернуть самое большое из четырех чисел.
 // function findMaxNumber(a, b, c, d) {
 //   if (typeof a !== 'number' ||
@@ -281,7 +231,6 @@ showMyDB(personalMovieDB.privat);
 // }
 
 // console.log(findMaxNumber(21, 4, 7.5, 24));
-
 
 // Задача. Вернуть строку, в которой выведены числа Фибоначчи.
 // function fib(num) {
@@ -309,181 +258,3 @@ showMyDB(personalMovieDB.privat);
 // }
 
 // console.log(fib(7));
-
-
-// Udemy 31. Объекты. Деструктурицазия объектов.
-// const user = {
-//   name: 'Valera',
-//   age: 29,
-//   isMarried: true,
-//   hobbies: {
-//     guitar: true,
-//     drawing: true,
-//   },
-// };
-
-// Метод перебора объектов 'for in'.
-// for (let key in user) {
-//   if (typeof user[key] === 'object') {
-//     for (let k in user[key]) {
-//       console.log(`Свойство ${k} имеет значение ${user[key][k]}`);
-//     }
-//   } else {
-//     console.log(`Свойство ${key} имеет значение ${user[key]}`);
-//   }
-// }
-
-// // Деструктуризация
-// const { guitar, drawing } = user.hobbies;
-
-// console.log(Object.keys(user)); // -> ['name', 'age', 'isMarried', 'hobbies']
-
-
-// Udemy 32. Массивы, псевдомассивы.
-// const arr = [2, 34, 5, 'str', false];
-
-// arr.pop();
-// arr.push(240);
-// console.log(arr);
-
-// // Метод перебора массивов 'for of'.
-// for (let value of arr) {
-//   console.log(value);
-// }
-
-// Метод перебора массивов 'forEach()'.
-// arr.forEach((item, i) => {
-//   if (typeof item === 'number') {
-//     console.log(`Элемент ${item * 2} под индексом ${i}`);
-//   } else {
-//     console.log(`Элемент ${item} под индексом ${i}`);
-//   }
-// });
-
-// const str = 'aaa, fff, ddd, kkkkk, bbb, sss, ccc';
-// const arrOfWords = str.split(', '); // Метод split(). Разбивает строку на элементы массива.
-// console.log(arrOfWords);
-
-// console.log(arrOfWords.join('; ')); // Метод join(). Склеивает элементы массива в строку.
-
-// console.log(arrOfWords.sort()); // Метод sort(). Сортирует элементы массива.
-// // Чтобы метод правильно работал с числами, передаем в него callback.
-// const arrOfNumbers = [25, 2, 234, 41, 1, 64, 111, 33];
-// function compareNum(a, b) {
-//   return a - b;
-// }
-
-// console.log(arrOfNumbers.sort(compareNum));
-
-
-// Udemy 34. Передача по ссылке и по значению.
-// По значению (примитивы).
-// const numA = 10;
-// const numB = numA + 20;
-// console.log(`a - ${numA}, b - ${numB}`); // -> a - 10, b - 30
-
-// По ссылке (объекты).
-const user = {
-  name: 'Nik',
-  age: 30,
-  isMarried: true,
-  hobbies: [
-    'guitar',
-    'workout',
-    'books',
-  ],
-};
-
-// Неглубокая копия объекта при помощи for in.
-// function makeUserCopy(mainUser) {
-//   let userCopy = {};
-
-//   for (let key in mainUser) {
-//     userCopy[key] = mainUser[key];
-//   }
-
-//   return userCopy;
-// }
-
-// const userCopy = makeUserCopy(user);
-// userCopy.name = 'Alex';
-// console.log(user);
-// console.log(userCopy);
-
-// Неглубокая копия объекта при помощи Object.assign().
-// const objCopy = Object.assign({}, user);
-// objCopy.name = 'Wilson';
-// console.log(objCopy);
-
-// Неглубокая копия объекта при помощи spread - оператора.
-// const objCopy = { ...user };
-// objCopy.name = 'Jack';
-// console.log(objCopy);
-
-// Глубокая копия объекта при помощи JSON.
-// const userCopy = JSON.parse(JSON.stringify(user));
-// userCopy.hobbies[2] = 'running';
-// console.log(userCopy);
-
-// Копирование массивов: for, for of, slice(), spread, JSON.
-// const arr = [1, 33, 22, 55, 44];
-// console.log(arr);
-
-// function makeArrCopy(array) {
-//   const copy = [];
-
-//   for (let i = 0; i < array.length; i++) {
-//     copy[i] = array[i];
-//   }
-
-//   return copy;
-// }
-
-// const arrCopy = makeArrCopy(arr);
-
-// const forOfArrCopy = [];
-// for (let value of arr) {
-//   arrCopy.push(value);
-// }
-
-// const sliceArrCopy = arr.slice('');
-// const spreadArrCopy = [...arr];
-// const arrCopy = JSON.parse(JSON.stringify(arr));
-
-// Task 10.
-const personalPlanPeter = {
-  name: "Peter",
-  age: "30",
-  skills: {
-    languages: ['ru', 'ua'],
-    programmingLangs: {
-      js: '20%',
-      php: '10%'
-    },
-    exp: '1 month'
-  },
-};
-
-function showExperience(plan) {
-  const { exp } = plan.skills;
-
-  return exp;
-}
-
-console.log(showExperience(personalPlanPeter));
-
-function showProgrammingLangs(plan) {
-  let str = '';
-  const { programmingLangs } = plan.skills;
-
-  for (let key in programmingLangs) {
-    if (key) {
-      str += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
-    } else {
-      return str;
-    }
-  }
-  return str;
-}
-
-console.log(showProgrammingLangs(personalPlanPeter));
